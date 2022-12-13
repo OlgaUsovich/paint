32d844e5c16cb4df6127f6e4ef8be3ce01a1f782
Olga Usovich <olga.usovich.17@mail.ru>
2022-12-13 21:17:15 +0300

    feat: add store

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,7 +1,18 @@
-import "../styles/canvas.scss"
+import { observer } from "mobx-react-lite";
+import { useEffect, useRef } from "react";
+import canvasState from "../store/canvasState";
+import "../styles/canvas.scss";
 
-export const Canvas = () => {
-    return <div className="canvas">
-        <canvas width={800} height={500}/>
+export const Canvas = observer(() => {
+  const canvasRef = useRef() as React.MutableRefObject<HTMLCanvasElement>;
+
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current);
+  }, []);
+
+  return (
+    <div className="canvas">
+      <canvas ref={canvasRef} width={800} height={500} />
     </div>
-}
+  );
+});
