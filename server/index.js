32c7531dd5e0677c8a37d334b8964ee116a4a966
OLGA <OLGA@example.com>
2022-12-24 20:10:42 +0300

    feat: configure server part

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const WSserver = require("express-ws")(app);
+const aWss = WSserver.getWss();
+
+const PORT = process.env.PORT || 5000;
+
+app.ws("/", (ws, req) => {
+    console.log("ПОДКЛЮЧЕНИЕ УСТАНОВЛЕНО");
+    ws.send("Ты успешно подключился");
+    ws.on("message", (msg) => {
+        msg = JSON.parse(msg);
+        switch (msg.method) {
+            case "connection":
+                connectionHandler(ws, msg);
+                break;
+        }
+    });
+});
+
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+
+const connectionHandler = (ws, msg) => {
+    ws.id = msg.id;
+    broadcastConnection(ws, msg);
+};
+
+const broadcastConnection = (ws, msg) => {
+    aWss.clients.forEach((client) => {
+        if (client.id === msg.id) {
+            client.send(`Пользователь ${msg.username} подключен`);
+        }
+    });
+};
