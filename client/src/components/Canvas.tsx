19bf0a50831e2e7838cf6785fe98b2ffff93a2cd
OLGA <OLGA@example.com>
2022-12-24 22:14:55 +0300

    feat: add web socket onnection

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { useEffect, useRef, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import canvasState from "../store/canvasState";
 import toolState from "../store/toolState";
 import "../styles/canvas.scss";
@@ -10,11 +11,29 @@ export const Canvas = observer(() => {
   const canvasRef = useRef() as React.MutableRefObject<HTMLCanvasElement>;
   const usernameRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const [modal, setModal] = useState<boolean>(true);
+  const params = useParams();
 
   useEffect(() => {
     canvasState.setCanvas(canvasRef.current);
     toolState.setTool(new Brush(canvasRef.current));
   }, []);
+
+  useEffect(() => {
+    if  (canvasState.username) {
+      const socket = new WebSocket('ws://localhost:5000');
+      socket.onopen = () => {
+        console.log('hoijoijo')
+        socket.send(JSON.stringify({
+          id: params.id,
+          username: canvasState.username,
+          method: 'connection'
+        }))
+      }
+      socket.onmessage = (event) => {
+        console.log(event.data)
+      }
+    }
+  }, [canvasState.username]);
 
   const mouseDownHandler = () => {
     canvasState.pushToUndo(canvasRef.current.toDataURL())
