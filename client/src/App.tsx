97ba9f09352d54c65172cf569eb8ce0a6c1504b4
OLGA <OLGA@example.com>
2022-12-24 21:01:44 +0300

    feat: add routes with unique user id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import './styles/app.scss';
-import {ToolBar} from "./components/ToolBar"
-import {SettingBar} from "./components/SettingBar"
-import {Canvas} from "./components/Canvas"
+import React from "react";
+import "./styles/app.scss";
+import { ToolBar } from "./components/ToolBar";
+import { SettingBar } from "./components/SettingBar";
+import { Canvas } from "./components/Canvas";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app">
-      <ToolBar />
-      <SettingBar />
-      <Canvas />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/:id"
+            element={
+              <>
+                <ToolBar />
+                <SettingBar />
+                <Canvas />
+              </>
+            }
+          />
+          <Route element={<Navigate to={`f${(+new Date()).toString(16)}`} />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
