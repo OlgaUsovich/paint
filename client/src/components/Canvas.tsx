f0f2090add79a818f65aebf4804c61eb0b9a89b8
Olga Usovich <olga.usovich.17@mail.ru>
2023-01-02 22:24:46 +0300

    fix: fix gitting context issue

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -17,14 +17,17 @@ export const Canvas = observer(() => {
 
   useEffect(() => {
     canvasState.setCanvas(canvasRef.current);
+    let ctx = canvasRef.current.getContext('2d')
     axios
       .get(`http://localhost:5000/image?id=${params.id}`)
       .then((response) => {
         const img = new Image();
         img.src = response.data;
         img.onload = () => {
-          this.ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-          this.ctx.drawImage(img, 0, 0, canvasRef.current.width, canvasRef.current.height);
+          if (ctx) {
+            ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            ctx.drawImage(img, 0, 0, canvasRef.current.width, canvasRef.current.height);
+          }
         };
       });
   }, []);
